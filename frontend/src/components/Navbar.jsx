6d79aa35b2b748ceff8844b3094cfa7a7232041d
bibaswankundu123 <bibaswankundu4@gmail.com>
2025-05-20 18:20:36 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,8 +10,7 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-      {/* <img onClick={() => navigate('/')} className="w-44 cursor-pointer" src={assets.logo} alt="Logo" /> */}
-      <p className="text-blue-800 font-extrabold text-3xl ">M/S Medicaid</p>
+      <img onClick={() => navigate('/')} className="w-44 cursor-pointer" src={assets.logo} alt="Logo" />
       <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to="/">
           <li className="py-1">HOME</li>
