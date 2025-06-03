c434038ed9f9702aaa8f1a4f1d99026b775966d5
bibaswankundu123 <bibaswankundu4@gmail.com>
2025-06-03 07:51:53 +0530

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,33 +1,143 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import {v2 as cloudinary} from 'cloudinary'
 
-import userService from "../services/userService.js"
+//API to register a new user
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-//Bearer token
-export const getUserProfile = async(req,res)=> {
+    if (!name || !password || !email) {
+      return res.json({
+        success: false,
+        message: "Missing Details",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Invalid Email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Enter a strong password",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name: name,
+      email: email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "user does not exist",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+const getProfile = async (req,res) => {
    try{
-    const jwt=req.headers.authorization?.split(" ")[1];
-     
-    if(!jwt){
-        return res.status(404).send({error:"token not found"})
-    }
-    const user = await userService.getUserProfileByToken(jwt);
 
-    return res.status(200).send(user);
+     const  {userId} = req.body
+     const userData = await userModel.findById(userId).select('-password')
 
-   }catch (error){
-      return res.status(500).send({error:error.message});
+     res.json({success:true,userData})
+   }catch (error) {
+        console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
    }
-
-
 }
 
 
-export const getAllUsers = async(req,res)=> {
-    try{
-        const users = await userService.getAllUsers();
-        return res.status(200).send(users);
-    }catch (error){
-        return res.status(500).send({error:error.message});
-    }
+
+const updateProfile = async (req,res) => {
+   try {
+    
+     const {userId,name,phone,address,dob,gender} = req.body
+
+    const imageFile = req.file
+     if (!name || !phone  || !dob || !gender) {
+
+      return res.json({success:false,message:"Data Missing"})
+      
+     }
+
+     await userModel.findByIdAndUpdate(userId,{name,phone,address:JSON.parse(address),dob,gender})
+
+     if (imageFile) {
+
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+        const imageURL = imageUpload.secure_url
+
+        await userModel.findByIdAndUpdate(userId,{image:imageURL})
+      
+     }
+
+     res.json({success:true,message:"Profile Updated"})
+
+
+
+   } catch (error) {
+     console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+    
+   }
 }
 
-export default { getUserProfile, getAllUsers };
+export { registerUser, loginUser ,getProfile,updateProfile};
